238ea3e681639c79b460919d357e2738cc392ab4
Fran <tu-email@example.com>
2025-12-24 19:34:00 -0300

    Preparar para deploy a GitHub Pages

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
  import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
-import Home from './pages/Home'; // Asegúrate de crear este componente
-import Game from './pages/Game'; // Asegúrate de crear este componente
+import Home from './pages/Home';
+import Game from './pages/Game';
 
 const App = () => {
-return (
-<Router>
-<Switch>
-  <Route exact path="/" component={Home} />
-  <Route path="/game" component={Game} />
-</Switch>
-</Router>
-);
+  return (
+    <Router basename="/desafio-piedra">
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/game" component={Game} />
+      </Switch>
+    </Router>
+  );
 };
 
 export default App;
